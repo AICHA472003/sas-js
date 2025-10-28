a7887766b043f5e23944bb9bc50e240b56a8fb24
AICHA472003 <dhiminaicha7@gmail.com>
2025-10-28 15:22:31 +0100

    Add files via upload

---
+++ interaction avec utilisation/script.js
@@ -0,0 +1,10 @@
+let bouton= document.getElementById("btn-start");
+let message= document.getElementById ("message");
+bouton.addEventListener("click", function () {
+let nom= prompt("Quel est votre nom ?") ;
+if (nom ) {
+    message.innerText="Bonjour,"+ nom +"!";
+}else{
+    alert("Aucun nom saisi !");
+}
+});
